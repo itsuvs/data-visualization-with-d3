0dd05ce8f06e1d6b71833338d1b4e3eaacd202d8
Ujjwal <itsujji@gmail.com>
2022-02-20 22:46:18 +0530

    Axes half completed

--- src/components/Axes.js
+++ src/components/Axes.js
@@ -12,7 +12,7 @@ const Axes = () => {
     var width = 272;
     var height = 272;
 
-    var svg = d3.select('#bar-chart')
+    var svg = d3.select('#axes')
                 .append('svg')
                 .attr('style',"outline: 1px solid black")
                 .attr('style',"width: 100%")
@@ -21,25 +21,15 @@ const Axes = () => {
                 .attr('style', 'outline: 1px solid black')
                 .attr('style', 'background: #e5e5e5');
 
-    var bars = svg.selectAll('.bar')
-                 .data(data)
-                 .enter()
-                 .append('rect');
-          
-    bars.attr("x", function(d,i){ return 10 + i * 40})
-        .attr("y", function(d){return height - d - 10})
-        .attr("width", 30)
-        .attr("height", function(d){ return d})
-        .attr("fill", "black");
+    var scale = d3.scaleLinear().domain([20, 220]).range([0, 400]);
 
-    var texts = svg.selectAll(".myTexts")
-                   .data(data)
-                   .enter()
-                   .append("text");
-                   
-    texts.attr("x", function(d,i){ return 12 + i * 40})
-        .attr("y", function(d){ return height - d - 15})
-        .text(function(d){ return d});
+    var xaxis = d3.axisBottom()
+    .scale(scale);
+    
+    svg.append("g")
+    .attr("transform", "translate(50,120)")
+    .call(xaxis);
+    
   },[])
 
   return (
@@ -48,10 +38,13 @@ const Axes = () => {
         Axes
       </Header>
       <p className={styles.content}>
-        Any sort of chart or graph is incomplete without axes as they provide human-readable scale references. D3 has four easy methods to draw four types of axes. All we need is an 'SVG' element to add the axes into and
+        Any sort of chart or graph is incomplete without axes as they provide human-readable scale references. D3 has four easy methods to draw four types of axes: axisTop, axisBottom, axisRight and axisLeft. All we need is an 'SVG' element to add the axes into and
         a scaling function to scale against given data. Talking about scaling function, there are total 6 of them: scaleLinear, scaleSqrt, scaleLog, scaleTime, scaleBand and scalePoint. I have included a picture at the bottom of this
         article which I think does the best job in depicting the key differences among them.
       </p>
+      <Segment className={styles.contentImage} id="axes">
+
+      </Segment>
     </Layout>
   );
 };
