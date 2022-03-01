5766ca73d1b17fd58742740cdae5fde893157f04
Ujjwal <itsujji@gmail.com>
2022-03-01 16:46:19 +0530

    Added info table for first scaling category

--- src/components/Scaling.js
+++ src/components/Scaling.js
@@ -1,33 +1,86 @@
-import React from 'react';
-import {Header, Segment, Image, Container} from 'semantic-ui-react';
-import styles from '../styles/Layout.module.css';
-import Layout from './Layout';
+import React from "react";
+import { Header, Segment, Image, Container, Table } from "semantic-ui-react";
+import styles from "../styles/Layout.module.css";
+import Layout from "./Layout";
 
 export default function Scaling() {
   return (
     <Layout>
       <Header>Scaling</Header>
       <p className={styles.content}>
-        Scaling functions transform data values into visual variables such as position, length, color, etc. In simple words, they describe how an actual dimension of the original data is to be represented as a visual variable.
-        D3 has about 12 scaling functions that can be grouped into three categories: 
+        Scaling functions transform data values into visual variables such as
+        position, length, color, etc. In simple words, they describe how an
+        actual dimension of the original data is to be represented as a visual
+        variable. D3 has about 12 scaling functions that can be grouped into
+        three categories:
       </p>
       <ul>
-          <li style={{fontWeight: 'bold', marginBottom:'8px'}}>
-            Scales with continous input and continous output
-          </li>
-          <li style={{fontWeight: 'bold', marginBottom:'8px'}}>
-            Scales with continous input and discreet output
-          </li>
-          <li style={{fontWeight: 'bold', marginBottom:'8px'}}>
-            Scales with discreet input and discreet output
-          </li>
-        </ul>
+        <li style={{ fontWeight: "bold", marginBottom: "8px" }}>
+          Scales with continuous input and continuous output
+        </li>
+        <li style={{ fontWeight: "bold", marginBottom: "8px" }}>
+          Scales with continuous input and discreet output
+        </li>
+        <li style={{ fontWeight: "bold", marginBottom: "8px" }}>
+          Scales with discreet input and discreet output
+        </li>
+      </ul>
       <Container>
-        <h4>
-         1. Scales with continous input and continous output
-        </h4>
+        <h4>1. Scales with continuous input and continuous output</h4>
+        
+        <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Function</Table.HeaderCell>
+              <Table.HeaderCell>Description</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell singleLine>scaleLinear()</Table.Cell>
+              <Table.Cell>
+                Uses y=mx+c to interpolate across domain and range
+              </Table.Cell>
+            </Table.Row>
+            
+            <Table.Row>
+              <Table.Cell singleLine>scalePow()</Table.Cell>
+              <Table.Cell>
+                Uses y=m*x^k + b to interpolate across domain and range
+              </Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+              <Table.Cell singleLine>scaleSqrt()</Table.Cell>
+              <Table.Cell>
+                This is the special case of power scale which use k = 0.5
+              </Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+              <Table.Cell singleLine>scaleLog()</Table.Cell>
+              <Table.Cell>
+                Uses y=m*log(x)+b for interpolation
+              </Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+              <Table.Cell singleLine>scaleTime()</Table.Cell>
+              <Table.Cell>
+                Similar to linear scale except the domain is expressed in dates
+              </Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+              <Table.Cell singleLine>scaleSequential()</Table.Cell>
+              <Table.Cell>
+                Uses custom function called as interpolator for interpolation
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </Container>
     </Layout>
-  )
+  );
 }
-
