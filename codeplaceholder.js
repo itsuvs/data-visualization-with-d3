3b4d0d43e6e99af9652571204b95321a67678206
Ujjwal <itsujji@gmail.com>
2022-04-06 14:05:00 +0530

    New changes to Hello World

--- codeplaceholder.js
+++ codeplaceholder.js
@@ -1,21 +1,30 @@
-var data = [85, 25, 152, 47, 200];
+d3.select('#hello-world')
+  .append('svg')
+  .attr('style',"outline: 1px solid black; width: 100%; background: black;")
+  .append('text')
+  .text('Hello World')
+  .attr("x", 20)
+  .attr("y", 90)
+  .attr('style',"fill: yellow; font-size: 36; font-family: monospace;")
 
-var width = 272;
-var height = 272;
+// var data = [85, 25, 152, 47, 200];
 
-var svg = d3
-  .select("#axes")
-  .append("svg")
-  .attr("style", "outline: 1px solid black")
-  .attr("style", "width: 100%")
-  .attr("width", width)
-  .attr("height", height)
-  .attr("style", "outline: 1px solid black")
-  .attr("style", "background: #e5e5e5");
+// var width = 272;
+// var height = 272;
 
-var scale = d3.scaleLinear().domain([0, 200]).range([10, 252]);
+// var svg = d3
+//   .select("#axes")
+//   .append("svg")
+//   .attr("style", "outline: 1px solid black")
+//   .attr("style", "width: 100%")
+//   .attr("width", width)
+//   .attr("height", height)
+//   .attr("style", "outline: 1px solid black")
+//   .attr("style", "background: #e5e5e5");
 
-var xaxis = d3.axisBottom().scale(scale);
+// var scale = d3.scaleLinear().domain([0, 200]).range([10, 252]);
 
-svg.append("g").attr("transform", "translate(0,140)").call(xaxis);
+// var xaxis = d3.axisBottom().scale(scale);
+
+// svg.append("g").attr("transform", "translate(0,140)").call(xaxis);
 
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -29,9 +29,12 @@ const HelloWorld = () => {
         but for the sake of simplicity, understanding and my sanity I am using text.
       </p>
       <p className={styles.content}>
-        D3 is all about 'selections and binding'. You make selections in DOM and manipulate them using some predefined methods known as 'operators'. It's just like how we do it in classic DOM manipulation but
-        D3 offers a lot cleaner interface. Other than its ease of use, D3 is an open source library so we can do lots of experiment without worrying about api call limitations
-        or payments in general. Now, let's get into coding:
+        <span style={{'background': '#1e1e1e', 'color':'white', 'padding': '5px', 'marginBottom': '2px'}}>
+         What I am trying to achieve: 
+         </span>
+      </p>
+      <p>
+        To display text "Hello World" in an SVG element using D3
       </p>
       <p className={styles.subHeading}>
         Input:
@@ -41,7 +44,7 @@ const HelloWorld = () => {
       </Segment>
       <p className={styles.content}>
         As we can see above, I've selected an element <span className={styles.highlighter}>#hello-world</span> using D3's operator 'select'. The 'append' operator in 2nd line adds an svg inside <span className={styles.highlighter}>#hello-world</span> element.
-        The 'attr' operator in 3rd and 4th line assigns some attribute to the svg element. Then I appended a text element with text 'Hello World' and then assigned some attributes to it. So simple!
+        The 'attr' operator in the 3rd line assigns style attribute to the svg element. Then I appended a text element with text 'Hello World' and then defined it's coordinates and styling using 'attr' operator. Pretty easy this one!
       </p>
       <p className={styles.subHeading}>
         Output:
@@ -50,7 +53,9 @@ const HelloWorld = () => {
 
       </Segment>
       <p className={styles.content}>
-        This is quite an easy example but it's also a necessary one. I absolutely had fun learning this! 
+        D3 is all about 'selections and binding'. You make selections in DOM and manipulate them using some predefined methods known as 'operators'. It's just like how we do it in classic DOM manipulation but
+        D3 offers a lot cleaner interface. Other than its ease of use, D3 is an open source library so we can do lots of experiment without worrying about api call limitations
+        or payments in general.
       </p>
     </Layout>
   );
