c6f607ae9058e6019e230122d736bb9fe771dacf
Ujjwal <itsujji@gmail.com>
2022-04-18 23:16:00 +0530

    Changes made to Axes blog

--- src/components/Axes.js
+++ src/components/Axes.js
@@ -40,22 +40,19 @@ const Axes = () => {
           axisTop, axisBottom, axisRight and axisLeft
         </span>
         . All we need is an 'SVG' element to add the axes into and a scaling
-        function to scale against given data 
-        {/* Talking about scaling functions,
-        there are four categories of them: Continuous, Sequential, Quantized and
-        Ordinal, but I am going to discuss only the ones in Continuous category:{" "}
-        <span
-          style={{ fontStyle: "italic", color: "purple", fontWeight: "bold" }}
-        >
-          scaleLinear, scaleSqrt, scaleLog, scaleTime, scaleBand and scalePoint
-        </span> */}
-        . Let's get into code.
+        function to scale against given data. Let's get into code.
+      </p>
+      <p className={styles.mission}>
+         What I am trying to achieve : 
+      </p>
+      <p className={styles.missionStatement}>
+        Display a horizontal axis that can scale from 0 to 200
       </p>
       <Segment secondary compact>
         <Image className={styles.contentImage} src={AxesImage} fluid />
       </Segment>
       <p className={styles.content}>
-        From line 1 to 14 is pretty easy to understand; in line 16 we have added
+        From line 1 to 14, it's same as we learned in blog "Hello World" and "Bar Chart";. In line 16, I have introduced
         three new terms:{" "}
         <span
           style={{ fontStyle: "italic", color: "purple", fontWeight: "bold" }}
@@ -63,9 +60,9 @@ const Axes = () => {
           domain, range & scaleLinear
         </span>
         . Domain is essentially the extremes of data we would like to plot and
-        range is how we want to spread the axes against the dimensions of the
-        svg element. Talking about scaleLinear, it's one of the scaling functions 
-        which I will cover in the next blog.
+        range is how we want to spread the axis against the dimensions of the
+        SVG element. Talking about scaleLinear, it's one of the scaling functions 
+        which creates scale with a linear relationship between input and output.
       </p>
       <Segment className={styles.contentImage} id="axes" compact></Segment>
     </Layout>
